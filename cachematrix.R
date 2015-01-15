50b656a8beb6627c0333686eab1b377ba4485621
Katja <katjakk@saunalahti.fi>
2015-01-15 23:05:52 +0200

    initial

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix takes a matrix as an argument that is assummed
+# to be always inverseable (solve(x)). It stores the matrix
+# and provides get function to retrieve the matrix and 
+# getinverse to retrieve its cached inversion which is initialized to NULL.
+# setinverse sets the cache value and set sets the new matrix.
 makeCacheMatrix <- function(x = matrix()) {
   c <- NULL
   set <- function(y) {
@@ -20,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+# cacheSolve takes makeCacheMatrix object as a function argument and
+# get its cached inversion and returns it. If it is null cacheSolve
+# then calculates and returns it, and sets the value to makeCacheMatrix object.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   c <- x$getinverse()
